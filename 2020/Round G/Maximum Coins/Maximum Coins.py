860c210dd02ebf7e1578edc55f015d0b7ad59b2a
marialysyuk <44041257+marialysyuk@users.noreply.github.com>
2021-02-12 16:03:31 +0300

    Add files via upload

---
+++ 2020/Round G/Maximum Coins/Maximum Coins.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def diagonal_sum(matrix):
+    length = len(matrix[0])
+    fin = 0
+    temp = 0
+    for j in range(length-1, -1, -1):
+        i = 0
+        while i < length and j < length:
+            temp += matrix[i][j]
+            i += 1
+            j += 1 
+        if temp > fin:
+            fin = temp
+        i = 0
+        temp = 0
+    for j in range(length-1, -1, -1):
+        i = 0
+        while i < length and j < length:
+            temp += matrix[j][i]
+            i += 1
+            j += 1 
+        if temp > fin:
+            fin = temp
+        i = 0
+        temp = 0
+    return fin
+    
+import sys
+nums = sys.stdin.readlines()
+
+matrix = [[int(num) for num in line.split()] for line in nums]
+matrix = matrix[1:]
+i = 0
+cases = 0
+while i < len(matrix):
+    j = matrix[i][0]
+    temp =[]
+    for k in range(i+1,i+1+j):
+        temp.append(matrix[k])
+    cases += 1
+    if len(temp) > 1:
+        print("Case #"+str(cases)+": "+str(diagonal_sum(temp)))
+    else:
+        print("Case #"+str(cases)+": "+str(temp[0][0]))
+    i += j+1
+

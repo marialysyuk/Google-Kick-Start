94e9d965e47f78a53eee4c4bcc0b3a27bc364ddb
marialysyuk <44041257+marialysyuk@users.noreply.github.com>
2021-02-12 22:58:07 +0300

    Add files via upload

---
+++ 2020/Round H/Retype/Retype.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+nums = sys.stdin.readlines()
+matrix = [[int(num) for num in line.split()] for line in nums]
+amount = int(matrix[0][0])
+nums = matrix[1:]
+
+for i in range(len(nums)):
+    print("Case #"+str(i+1)+": "+str(min((nums[i][1]+nums[i][0]), (2*(nums[i][1]-nums[i][2])+nums[i][0]))))
+    
+

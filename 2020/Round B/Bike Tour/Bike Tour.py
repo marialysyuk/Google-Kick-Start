a6687f1472d32317cae741397ae78f9e638d08ce
marialysyuk <44041257+marialysyuk@users.noreply.github.com>
2021-03-23 18:09:35 +0300

    Add files via upload

---
+++ 2020/Round B/Bike Tour/Bike Tour.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+
+def peaks(matrix):
+    count = 0
+    i = 1
+    while i < len(matrix)-1:
+        if (matrix[i-1] < matrix[i]) and (matrix[i+1] < matrix[i]):
+            count += 1
+        i += 1
+    return count
+
+nums = sys.stdin.readlines()
+matrix = [[int(num) for num in line.split()] for line in nums]
+matrix = matrix[1:]
+cases = 0
+i = 0 
+while i < len(matrix):
+    cases += 1
+    print("Case #"+str(cases)+": "+str(peaks(matrix[i+1])))
+    i += 2
+
